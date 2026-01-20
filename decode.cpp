65bce02c35fffb90a5f2ee0b0153459a1b51f4eb
rusperres <jairusjaspervc@gmail.com>
2026-01-20 22:49:15 +0800

    started creating torrent file parser

---
+++ decode.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+int main()
+
+struct TorrentItem{
+	std::string key;
+	std::string value;
+};
+struct TorrentItemList{
+	std::vector<TorrentIten> list;
+};
+{	
+	std::string bytes = "d8:announce33:http://192.168.1.17:9000/announce10:created by18:qBittorrent v5.1.413:creation datei1768735040e4:infod9:file treed8:test.txtd0:d6:lengthi11e11:pieces root32:Ð⑿:_ù*xÏÃfÁ?±#ë)ÁÊ7ÇñB][BMeee6:lengthi11e12:meta versioni2e4:name8:test.txt12:piece lengthi16384e6:pieces20:çP␃/;Â¨ß␝ôvøï␃Caúe12:piece layersdeee";
+	int n =bytes.length();
+
+
+}
