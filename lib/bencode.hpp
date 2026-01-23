21841a9602964b65eb0ae2dff5f5a380b1cbae5e
rusperres <jairusjaspervc@gmail.com>
2026-01-23 20:23:12 +0800

    Started bencode.hpp

---
+++ lib/bencode.hpp
@@ -0,0 +1,16 @@
+class BEncode{
+	constexpr uint8_t dictionary_start = 'd';
+	constexpr uint8_t dictionary_end = 'e';
+	constexpr uint8_t list_start = 'l';
+	constexpr uint8_t list_end = 'e';
+	constexpr uint8_t number_start = 'i';
+	constexpr uint8_t number_end = 'e';
+	constexpr uint8_t byte_array_divider = ':';
+
+	public:
+	
+	std::string decode(const std::string& str){
+		
+	}
+
+};
