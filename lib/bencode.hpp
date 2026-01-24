1269ce9f06f058278866ef3101dd3c21ea08d2f3
rusperres <jairusjaspervc@gmail.com>
2026-01-24 17:00:55 +0800

    fixed stream bugs

--- lib/bencode.hpp
+++ lib/bencode.hpp
@@ -1,32 +1,38 @@
 struct bstream {
 	const uint8_t* curr;
 	const uint8_t* end;
-	uint8_t peek() { 
+	uint8_t peek() const { 
 		if ( curr == end ) throw std::runtime_error("EOF");
 		return *curr;
 	}
 	uint8_t next() { 
-		if ( curr == end ) throw std::runtime_error("EOF"
+		if ( curr == end ) throw std::runtime_error("EOF")
 		return *curr++; 
 	}
 	bstream(const std::vector<uint8_t>& bytes)
 		: curr(bytes.data()), end(bytes.data() + bytes.size()){}
 };
-using decoded = std::variant<
-	std::map<std::string, decoded>,
-	std::vector<decoded>
-	long,
-	std::vector<uint8_t>
->;
+struct decoded;
+using dict_t = std::map<std::string, decoded>;
+using list_t = std::vector<decoded>;
+struct decoded{
+	using value_t = std::variant<
+		dict_t,
+		list_t,
+		long,
+		std::vector<uint8_t>
+	value_t value;
+};
+
 
 class BEncode{
-	constexpr uint8_t dict_start = 'd';
-	constexpr uint8_t dict_end = 'e';
-	constexpr uint8_t list_start = 'l';
-	constexpr uint8_t list_end = 'e';
-	constexpr uint8_t num_start = 'i';
-	constexpr uint8_t num_end = 'e';
-	constexpr uint8_t byte_array_divider = ':';
+	static constexpr uint8_t dict_start = 'd';
+	static constexpr uint8_t dict_end = 'e';
+	static constexpr uint8_t list_start = 'l';
+	static constexpr uint8_t list_end = 'e';
+	static constexpr uint8_t num_start = 'i';
+	static constexpr uint8_t num_end = 'e';
+	static constexpr uint8_t byte_array_divider = ':';
 
 	decoded decode(const std::vector<uint8_t>& bytes){
 		bstream stream(bytes);
@@ -36,9 +42,9 @@ class BEncode{
 	decoded decode_next_obj(bstream& stream){
 		if ( stream.curr == stream.end )
 			throw std::runtime_error("Unexpected end of output");
-		if( stream.peek() == dict_start) return decode_dict(stream);
-		if( stream.peek() == list_start) return decode_list(stream);
-		if( stream.peek() == num_start) return decode_num(stream);
+		if( stream.peek() == dict_start) return decode_dict(stream.next());
+		if( stream.peek() == list_start) return decode_list(stream.next());
+		if( stream.peek() == num_start) return decode_num(stream.next());
 		return decode_byte_array(stream);
 	}
 
