2943789fdac87c917578f01a8a32a9c29d29e20b
rusperres <jairusjaspervc@gmail.com>
2026-01-24 16:40:47 +0800

    created decode_next_obj using bstream enumerator

--- lib/bencode.hpp
+++ lib/bencode.hpp
@@ -1,5 +1,10 @@
-using byte_it = std::vector<uint8_t>::const_iterator;
-
+struct bstream {
+	const uint8_t* curr;
+	const uint8_t* end;
+	uint8_t peek() { return * cur;}
+	uint8_t next() { return *cur++; }
+	bstream(const auto& bytes){ *cur = bytes.begin(), *end = bytes.end();}
+};
 class BEncode{
 	constexpr uint8_t dict_start = 'd';
 	constexpr uint8_t dict_end = 'e';
@@ -9,18 +14,18 @@ class BEncode{
 	constexpr uint8_t num_end = 'e';
 	constexpr uint8_t byte_array_divider = ':';
 
-	std::any decode(const auto& bytes){
-		auto it = bytes.begin();
-		return decode_next_obj(it, bytes.end());		
+	std::variant<std::orderered_set, std::vector, long int, std::vector<uint8_t>> decode(const auto& bytes){
+		bstream enumerator = bstream(bytes);
+		return decode_next_obj(enumerator);		
 	}
 
-	std::any decode_next_obj(byte_it& it, byte_it end){
+	stdd::variant<std::orderered_set, std::vector, long int, std::vector<uint8_t> decode_next_obj(bstream enumerator){
 		if ( it == end )
 			throw std::runtime_error("Unexpected end of output");
-		if( *it == dict_start) return decode_dict(it, end);
-		if( *it == list_start) return decode_list(it, end);
-		if( *it == num_start) return decode_num(it, end);
-		return decode_byte_array(it,end);
+		if( *it == dict_start) return decode_dict(enumerator);
+		if( *it == list_start) return decode_list(enumerator);
+		if( *it == num_start) return decode_num(enumerator);
+		return decode_byte_array(enumerator);
 	}
 
 };
